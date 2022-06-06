efd03e83545c708d265a6495c63ada50f54518cb
marekw1986 <marekw1986@gmail.com>
2022-06-06 12:49:42 +0200

    Minor changes

--- code/main/http_server.c
+++ code/main/http_server.c
@@ -91,11 +91,51 @@ esp_err_t hello_get_handler(httpd_req_t *req)
 }
 
 
+esp_err_t setmode_cgi_get_handler(httpd_req_t *req)
+{
+    char*  buf;
+    size_t buf_len;
+
+    /* Read URL query string length and allocate memory for length + 1,
+     * extra byte for null termination */
+    buf_len = httpd_req_get_url_query_len(req) + 1;
+    if (buf_len > 1) {
+        buf = malloc(buf_len);
+        if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) {
+            ESP_LOGI(TAG, "Found URL query => %s", buf);
+            char param[32];
+            /* Get value of expected key from query string */
+            if (httpd_query_key_value(buf, "mode", param, sizeof(param)) == ESP_OK) {
+                ESP_LOGI(TAG, "Found URL query parameter => query1=%s", param);
+            }
+        }
+        free(buf);
+    }
+
+    /* Set some custom headers */
+    httpd_resp_set_hdr(req, "Custom-Header-1", "Custom-Value-1");
+    httpd_resp_set_hdr(req, "Custom-Header-2", "Custom-Value-2");
+
+    /* Send response with custom headers and body set as the
+     * string passed in user context*/
+    const char* resp_str = (const char*) req->user_ctx;
+    httpd_resp_send(req, resp_str, strlen(resp_str));
+
+    /* After sending the HTTP response the old HTTP request
+     * headers are lost. Check if HTTP request headers can be read now. */
+    if (httpd_req_get_hdr_value_len(req, "Host") == 0) {
+        ESP_LOGI(TAG, "Request headers lost");
+    }
+    return ESP_OK;
+}
+
+
 esp_err_t data_json_get_handler(httpd_req_t *req)
 {
     char *data;
     data = constructJSON();
     if (data) {
+        httpd_resp_set_hdr(req, "Content-Type", "application/json");
         httpd_resp_send(req, data, strlen(data));
         free(data);
         
@@ -122,6 +162,15 @@ esp_err_t file_get_handler(httpd_req_t *req)
 			bytes_read = fread(data, 1, sizeof(data), f);
 			httpd_resp_send_chunk(req, data, bytes_read);
 		} while (bytes_read == sizeof(data));
+        char *ext = strchr(req->user_ctx, '.');
+        if (ext) {
+            if (strcmp(ext, "js") == 0) {
+                httpd_resp_set_hdr(req, "Content-Type", "application/javascript");
+            }
+            else if (strcmp(ext, "json") == 0) {
+                httpd_resp_set_hdr(req, "Content-Type", "application/json");
+            }
+        }
 		httpd_resp_send_chunk(req, NULL, 0);
 		fclose(f);
 		/* After sending the HTTP response the old HTTP request
@@ -168,6 +217,7 @@ esp_err_t sysinfo_get_handler(httpd_req_t *req)
 	cJSON_AddNumberToObject(root, "min_free_heap", esp_get_minimum_free_heap_size());
 	data = cJSON_Print(root);
 	cJSON_Delete(root);
+    httpd_resp_set_hdr(req, "Content-Type", "application/json");
     httpd_resp_send(req, data, strlen(data));
     free(data);
 
@@ -241,6 +291,15 @@ httpd_uri_t hello = {
     /* Let's pass response string in user
      * context to demonstrate it's usage */
     .user_ctx  = "Hello World!"
+};
+
+httpd_uri_t setmode_cgi_get = {
+    .uri       = "/ui/setmode.cgi",
+    .method    = HTTP_GET,
+    .handler   = setmode_cgi_get_handler,
+    /* Let's pass response string in user
+     * context to demonstrate it's usage */
+    .user_ctx  = "/spiffs/ui/setmode.cgi"
 };
 
 
@@ -410,6 +469,7 @@ httpd_handle_t start_webserver(void)
         ESP_LOGI(TAG, "Registering URI handlers");
         httpd_register_uri_handler(server, &hello);
         httpd_register_uri_handler(server, &data_json);
+        httpd_register_uri_handler(server, &setmode_cgi_get);
         httpd_register_uri_handler(server, &sysinfo_json);
         httpd_register_uri_handler(server, &wifiinfo_json);
         httpd_register_uri_handler(server, &token_get);

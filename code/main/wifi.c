b49ecf3d4f10727b5ffccde5314a4d68437e0b2d
Marek Wiecek <marekw1986@gmail.com>
2022-06-20 22:44:26 +0200

    Some changes

--- code/main/wifi.c
+++ code/main/wifi.c
@@ -26,38 +26,43 @@ static int s_retry_num = 0;
 static void event_handler(void* arg, esp_event_base_t event_base,
                                 int32_t event_id, void* event_data)
 {
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-        esp_wifi_connect();
-    }
-    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
-			mqtt_client_stop();
-            esp_wifi_connect();
-            s_retry_num++;
-            ESP_LOGI(TAG, "retry to connect to the AP");
-        } else {
-            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
-        }
-        ESP_LOGI(TAG,"connect to the AP fail");
-    }
-    else if (event_id == WIFI_EVENT_AP_STACONNECTED) {
-        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
-        ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
-                 MAC2STR(event->mac), event->aid);
-    }
-    else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
-        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
-        ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",
-                 MAC2STR(event->mac), event->aid);
-    }
-    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-        ESP_LOGI(TAG, "got ip:%s",
-                 ip4addr_ntoa(&event->ip_info.ip));
-        s_retry_num = 0;
-        mqtt_client_start();
-        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-    }    
+	if (event_base == WIFI_EVENT) {
+		if (event_id == WIFI_EVENT_STA_START) {
+			esp_wifi_connect();
+		}
+		else if (event_id == WIFI_EVENT_STA_DISCONNECTED) {
+			if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+				mqtt_client_stop();
+				esp_wifi_connect();
+				s_retry_num++;
+				ESP_LOGI(TAG, "retry to connect to the AP");
+			}
+			else {
+				xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+			}
+			ESP_LOGI(TAG,"connect to the AP fail");
+		}
+		else if (event_id == WIFI_EVENT_AP_STACONNECTED) {
+			wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
+			ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
+					 MAC2STR(event->mac), event->aid);
+		}
+		else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
+			wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
+			ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",
+					 MAC2STR(event->mac), event->aid);
+		}
+	}
+	else if (event_base == IP_EVENT) {
+		if (event_id == IP_EVENT_STA_GOT_IP) {
+			ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+			ESP_LOGI(TAG, "got ip:%s",
+					 ip4addr_ntoa(&event->ip_info.ip));
+			s_retry_num = 0;
+			mqtt_client_start();
+			xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+		}
+	}    
 }
 
 void wifi_init_sta(void) {
